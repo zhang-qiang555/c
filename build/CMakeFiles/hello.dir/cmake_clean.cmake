3aebe4c9ba54af55199fcb5ad4e56d7b34b60420
zhang-qiang555 <2662113933@qq.com>
2026-01-20 09:37:00 +0800

    first

---
+++ build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/learn/src/hello.cpp.obj"
+  "CMakeFiles/hello.dir/learn/src/hello.cpp.obj.d"
+  "D:/代码/学习/c/bin/hello.exe"
+  "D:/代码/学习/c/bin/hello.exe.manifest"
+  "D:/代码/学习/c/bin/hello.pdb"
+  "D:/代码/学习/c/bin/libhello.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
